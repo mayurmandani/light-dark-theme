2e0571e3407f9f46daea134b2c2752822e1d8e1b
Mayur Mandani <mmandani9@gmail.com>
2025-01-10 13:44:57 +0530

    Initial commit changes are pushed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+    <div className="main-container">
+    <h1 className="text-center"> Light Dark Theme App</h1>
+    </div>
+    </>
+  )
+}
+
+export default App;
